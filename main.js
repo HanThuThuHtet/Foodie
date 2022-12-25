5478d698e6da2d197c293b482b39dffdc6a37e38
hanthuthuhtet <htth5499@gmail.com>
2022-12-25 11:49:52 +0630

    about section and animation

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-import './style.scss'
+import './style.scss';
+import Typed from 'typed.js';
+import ScrollReveal from 'scrollreveal';
+
+//typed js
+var options = {
+    strings: ['Food ...', 'Drink ...'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 3000,
+    loop: true //infinity
+  };
+  
+  var typed = new Typed('.element', options);
+
+//scroll reveal
+  var toDown = {
+    distance: '50px',
+    origin: 'top',
+    interval: 500,
+    duration: 1000
+};
+ScrollReveal().reveal('.to-down', toDown);
+
+var toRight = {
+    distance: '50px',
+    origin: 'left',
+    interval: 300,
+    duration: 800
+};
+ScrollReveal().reveal('.to-right', toRight);
+
+var toLeft = {
+    distance: '50px',
+    origin: 'right',
+    interval: 300,
+    duration: 800
+};
+ScrollReveal().reveal('.to-left', toLeft);
+
